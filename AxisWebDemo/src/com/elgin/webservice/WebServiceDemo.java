d3baf4e8023aa77afeda7f8016181a95f9623da8
elgin <450675583@qq.com>
2016-01-08 16:03:39 +0800

    commit

---
+++ AxisWebDemo/src/com/elgin/webservice/WebServiceDemo.java
@@ -0,0 +1,12 @@
+package com.elgin.webservice;
+
+public class WebServiceDemo {
+   
+	public String  sayHello(String name){
+		return "hello " + name;
+	}
+	
+	public int getAge(){
+		return 26;
+	}
+}
